f3bdfca0e9a86688476a9321dca81c9be04ba08e
ra2dd <radoslawpryw@gmail.com>
2023-11-26 13:37:46 +0100

    add templates for register and login

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,11 +1,11 @@
 import functools
 
-form flask import (
+from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-for werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
     
-from flask.db import get_db
+from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
