42412d0adf86168599f653caeb8b14d6a887b251
ra2dd <radoslawpryw@gmail.com>
2023-12-06 12:56:14 +0100

    add tests for login, fix templates

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,6 +2,7 @@ import pytest
 from flask import g, session
 from flaskr.db import get_db
 
+
 def test_register(client, app):
     assert client.get('/auth/register').status_code == 200
     response = client.post(
@@ -14,6 +15,7 @@ def test_register(client, app):
             "SELECT * FROM user WHERE username = 'a'",
         ).fetchone() is not None
 
+
 @pytest.mark.parametrize(('username', 'password', 'message'), (
     ('', '', b'Username is required'),
     ('a', '', b'Password is required'),
@@ -25,3 +27,31 @@ def test_register_validate_input(client, username, password, message):
         data={'username': username, 'password': password}
     )
     assert message in response.data
+
+
+def test_login(client, auth):
+    assert client.get('/auth/login').status_code == 200
+    response = auth.login()
+    assert response.headers["Location"] == "/"
+
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == 'test'
+
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username.'),
+    ('test', 'a', b'Incorrect password.')
+))
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
+
+
+def test_logout(client, auth):
+    auth.login()
+
+    with client:
+        auth.logout()
+        assert 'user_id' not in session
